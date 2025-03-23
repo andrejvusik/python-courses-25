415406fd773b12ef838c2ff25c3b8731391b43a5
andrejvusik <andrejvusik@gmail.com>
2025-03-23 04:18:24 +0300

    Homework 1. Optimization of the code of task 11 based on the lecturer's comments

--- hw-001/task_11.py
+++ hw-001/task_11.py
@@ -2,17 +2,19 @@
 # Enter a string. Check if it is a valid IP address (the format
 # should be XXX.XXX.XXX.XXX, where XXX is a number from 0 to 255).
 
-test_string = input("Please enter the IP address to check if it is correct: ")
+testing_ip = input("Please enter the IP address to check if it is correct: ")
+valid_ip = 1
 
-if test_string.count(".") == 3:
-    test_list = test_string.split(".")
-    if test_list[0].isdigit() and test_list[1].isdigit() and test_list[2].isdigit() and test_list[3].isdigit():
-        if 0 <= int(test_list[0]) <= 255 and 0 <= int(test_list[1]) <= 255 and 0 <= int(
-                test_list[2]) <= 255 and 0 <= int(test_list[3]) <= 255:
-            print(f"The entered IP address \"{test_string}\" is correct.")
-        else:
-            print(f"The entered IP address \"{test_string}\" is NOT correct.")
-    else:
-        print(f"The entered IP address \"{test_string}\" is NOT correct.")
+if testing_ip.count(".") != 3:
+    print(f"The entered IP address \"{testing_ip}\" is NOT correct.")
+    valid_ip = 0
 else:
-    print(f"The entered IP address \"{test_string}\" is NOT correct.")
+    testing_ip_parts = testing_ip.split(".")
+    for part in testing_ip_parts:
+        if not part.isdigit() or not 0 <= int(part) <= 255:
+            print(f"The entered IP address \"{testing_ip}\" is NOT correct.")
+            valid_ip = 0
+            break
+
+if valid_ip:
+    print(f"The entered IP address \"{testing_ip}\" is correct.")
